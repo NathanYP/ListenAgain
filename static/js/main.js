01c5696e8891a8d30abbccabf48af6cedcd227c0
yyin <yinyp.sg@gmail.com>
2014-12-16 17:37:40 +0800

    init commit

--- static/js/main.js
+++ static/js/main.js
@@ -1,10 +1,32 @@
-$(document).ready(function() {
+"use strict";
 
-  var audio5js = new Audio5js({
-    ready: function() {
-      this.load('/file?name=22.mp3');
-      this.play();
-    }
+var app = angular.module('pocket', ['ngRoute'])
+
+  .controller('TingxieCtrl', function($scope) {
+
+  })
+
+  .config(function($routeProvider, $locationProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: '../partials/home.html'
+      })
+      .when('/tingxie', {
+        templateUrl: '../partials/tingxie-index.html',
+        controller: 'TingliCtrl'
+      })
+      .otherwise({ redirectTo: '/'});
+
+      $locationProvider.html5Mode(true);
   });
 
-});
+// $(document).ready(function() {
+
+//   var audio5js = new Audio5js({
+//     ready: function() {
+//       this.load('/file?name=22.mp3');
+//       this.play();
+//     }
+//   });
+
+// });
