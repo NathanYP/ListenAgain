6cd6e4f28f7e98945a7f266798570deb67cce7de
nathan.yyin@gmail.com <nathan.yyin@gmail.com>
2014-12-18 19:34:01 +0800

    test

--- scripts/download_elllo_resources.py
+++ scripts/download_elllo_resources.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 
+import sys
 import os.path
 import urllib2
 import re
 import json
 import logging
+import sqlite3
 
 from bs4 import BeautifulSoup
 
@@ -12,7 +14,29 @@ data_home = os.path.join(os.path.dirname(__file__), '..', 'data')
 index_file = os.path.join(data_home, 'elllo.json')
 data_folder = os.path.join(data_home, 'elllo')
 
+db_file = os.path.join(os.path.dirname(__file__), '..', 'data', 'app.db')
+db = sqlite3.connect(db_file)
+cursor = db.cursor()
 
+# db
+def create_db_table():
+  cursor.execute(''' 
+    create table if not exists audio_files ( 
+      id int primary key, category  varchar(255), title varchar(255), 
+      filename  varchar(255), url text)
+  ''')
+  db.commit()
+
+
+def has_downloaded(page):
+  cursor.execute('''SELECT count(*) from audio_files where url = ?''', (page,))
+  row = cursor.fetchone()
+  return row[0] == 1
+
+def insert_into(filename, page):
+  pass
+
+# crawler
 def get_soup(url):
   response = urllib2.urlopen(url)
   return BeautifulSoup(response.read())
@@ -35,47 +59,57 @@ def collect_index_pages():
       pages.add('http://www.elllo.org/english/' + item['href'])
     break
 
-  for p in pages:
-    parse_page(p)
-    break
+  return pages
 
 
-def parse_page(url):
-  soup = get_soup(url)
+def parse_and_download(page):
+  soup = get_soup(page)
 
+  audio_file = None
   ogg = soup.find('source', type='audio/ogg')
   if ogg:
-    download_audio(ogg['src'])
+    audio_file = download_audio(ogg['src'])
 
   transcript = list()
   container = soup.find('div', id='transcript', class_='transcript')
   for item in container.find_all('p'):
-    if len(item.strings) == 1:
-      transcript.append({'name': None, item.strings.join('')})
+    contents = list(item.stripped_strings)
+    if len(contents) == 1:
+      transcript.append({'name': None, 'transcript': ' '.join(contents)})
     else:
+      transcript.append({'name': contents[0], 'transcript': ' '.join(contents[1:])})
+
+  print(transcript)
 
 
-    # for string in item.strings:
-    #   print(repr(string))
-    # print('---')
+def download_audio(link):
+  filename = os.path.basename(link)
+  local_file = os.path.join(data_folder, filename)
+  CHUNK = 4 *1024
 
+  req = urllib2.urlopen(url)
+  with open(local_file, 'wb') as fp:
+    while True:
+      chunk = req.read(CHUNK)
+      if not chunk:
+        break
+      else:
+        fp.write(chunk)
 
-# def read_index_file():
-#   if not os.path.exists(index_file):
-#     with open(index_file, 'w') as file:
-#       json.dump(list(), file)
-
-#   existed = None
-#   with open(index_file, 'r') as file:
-#     existed = 
-
-
-# def write_index_file():
-#   pass
+  return local_file
 
 def main():
-  # existed = read_index_file()
-  collect_index_pages()
+  create_db_table()
+
+  all_pages = collect_index_pages()
+  for page in all_pages:
+    if not has_downloaded(page):
+      try:
+        print("{0} - downloading".format(page))
+        parse_and_download(page)
+      except:
+        print("{0} - failed, {1}".format(page, sys.exc_info()[0]))
+
 
 if __name__ == '__main__':
-  collect_index_pages()
+  main()
--- scripts/init_db.py
+++ scripts/init_db.py
@@ -5,37 +5,23 @@ import os.path
 
 
 db_file = os.path.join(os.path.dirname(__file__), '..', 'data', 'app.db')
+print('db file: %s', db_file)
 
-sql = """ 
-  drop table audio_files; \
-
-  create table if not exists audio_files (\
-    id        int primary key,\
-    category  varchar(255),\
-    title     varchar(255),\
-    filename  varchar(255),\
-    url       text\
-  );\
-
-  insert into audio_files(category, title, filename, url) values (\
-    'elllo.org', 'test', 'test', 'test'
-  );
-"""
-
-def main():
+def create_table_audios():
   db = sqlite3.connect(db_file)
   cur = db.cursor()
 
-  cur.executescript(sql)
-  print('table created')
+  db.execute(''' 
+    create table if not exists audio_files ( 
+      id int primary key, category  varchar(255), title varchar(255), 
+      ilename  varchar(255), url text)
+  ''')
   db.commit()
-
-  db.execute('SELECT count(*) from audio_files')
-  data = cur.fetchall()
-  print(len(data))
-
-  cur.close()
   db.close()
+
+
+def main():
+  create_table_audios()
 
 
 if __name__ == '__main__':
