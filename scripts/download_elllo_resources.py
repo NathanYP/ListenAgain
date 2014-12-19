d7768bae3906ef6d7d35f4a57a21b8f3e5e69f5c
yyin <yinyp.sg@gmail.com>
2014-12-19 09:01:53 +0800

    round one completed

--- scripts/download_elllo_resources.py
+++ scripts/download_elllo_resources.py
@@ -1,18 +1,21 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import os
+import re
 import os.path
 import urllib2
-import re
+import urlparse
 import json
 import logging
 import sqlite3
 
 from bs4 import BeautifulSoup
 
-data_home = os.path.join(os.path.dirname(__file__), '..', 'data')
-index_file = os.path.join(data_home, 'elllo.json')
-data_folder = os.path.join(data_home, 'elllo')
+
+data_folder = os.path.join(os.path.dirname(__file__), '..', 'data', 'elllo')
+if not os.path.exists(data_folder):
+  os.mkdir(data_folder)
 
 db_file = os.path.join(os.path.dirname(__file__), '..', 'data', 'app.db')
 db = sqlite3.connect(db_file)
@@ -22,19 +25,21 @@ cursor = db.cursor()
 def create_db_table():
   cursor.execute(''' 
     create table if not exists audio_files ( 
-      id int primary key, category  varchar(255), title varchar(255), 
-      filename  varchar(255), url text)
+      id integer primary key, category  varchar(255), title varchar(255), 
+      filename varchar(255), page_link text, transcript text)
   ''')
   db.commit()
 
 
-def has_downloaded(page):
-  cursor.execute('''SELECT count(*) from audio_files where url = ?''', (page,))
+def has_downloaded(page_link):
+  cursor.execute('''SELECT count(*) from audio_files where page_link = ?''', (page_link,))
   row = cursor.fetchone()
   return row[0] == 1
 
-def insert_into(filename, page):
-  pass
+def insert_into(category, filename, page_link, transcript):
+  cursor.execute('''INSERT INTO audio_files (category, title, filename, page_link, transcript) 
+                    VALUES (NULL, ?, ?, ?, ?)''', (category, filename, page_link, transcript))
+  db.commit()
 
 # crawler
 def get_soup(url):
@@ -70,16 +75,18 @@ def parse_and_download(page):
   if ogg:
     audio_file = download_audio(ogg['src'])
 
-  transcript = list()
-  container = soup.find('div', id='transcript', class_='transcript')
-  for item in container.find_all('p'):
-    contents = list(item.stripped_strings)
-    if len(contents) == 1:
-      transcript.append({'name': None, 'transcript': ' '.join(contents)})
-    else:
-      transcript.append({'name': contents[0], 'transcript': ' '.join(contents[1:])})
+    transcript = list()
+    container = soup.find('div', id='transcript', class_='transcript')
+    for item in container.find_all('p'):
+      contents = list(item.stripped_strings)
+      if len(contents) == 1:
+        transcript.append({'name': None, 'transcript': ' '.join(contents)})
+      else:
+        transcript.append({'name': contents[0], 'transcript': ' '.join(contents[1:])})
 
-  print(transcript)
+    return audio_file, transcript
+  else:
+    return None, None
 
 
 def download_audio(link):
@@ -87,7 +94,7 @@ def download_audio(link):
   local_file = os.path.join(data_folder, filename)
   CHUNK = 4 *1024
 
-  req = urllib2.urlopen(url)
+  req = urllib2.urlopen(link)
   with open(local_file, 'wb') as fp:
     while True:
       chunk = req.read(CHUNK)
@@ -98,17 +105,31 @@ def download_audio(link):
 
   return local_file
 
+
 def main():
   create_db_table()
 
   all_pages = collect_index_pages()
-  for page in all_pages:
+  for index, page in enumerate(all_pages):
     if not has_downloaded(page):
       try:
         print("{0} - downloading".format(page))
-        parse_and_download(page)
-      except:
-        print("{0} - failed, {1}".format(page, sys.exc_info()[0]))
+
+        audio_file, transcript = parse_and_download(page)
+        if audio_file:
+          uri = urlparse.urlparse(page)
+          filename = os.path.basename(audio_file)
+
+          insert_into(uri.netloc, filename, page, json.dumps(transcript))
+
+        print("{0} - downloaded\n".format(page))
+      except Exception as e:
+        print("{0} - failed".format(page))
+        print(e)
+        print("\n")
+
+    if index >= 5:
+      break
 
 
 if __name__ == '__main__':
